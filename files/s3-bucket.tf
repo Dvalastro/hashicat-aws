4e84eef1cf9dfdac3f723a813502a44a35c780ad
Daniel Valastro <Daniel@valastro.com.au>
2022-09-10 09:42:04 +0000

    Added s3 bucket module

--- files/s3-bucket.tf
+++ files/s3-bucket.tf
@@ -1,3 +1,9 @@
+module "s3-bucket" {
+  source  = "app.terraform.io/IAmSuperConfused/s3-bucket/aws"
+  version = "2.8.0"
+}
+
+
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
