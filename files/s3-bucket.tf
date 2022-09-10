35b15a246ddea7ebdc2f3d183f50566dc7a298bd
Daniel Valastro <Daniel@valastro.com.au>
2022-09-10 09:51:40 +0000

    Added s3 bucket module

--- files/s3-bucket.tf
+++ files/s3-bucket.tf
@@ -1,4 +1,8 @@
-module "s3-bucket" {
+//--------------------------------------------------------------------
+// Modules
+module "s3_bucket" {
   source  = "app.terraform.io/IAmSuperConfused/s3-bucket/aws"
-  version = "2.8.0"
+  version = "3.4.0"
+
+  bucket_prefix = "daniel-valastro"
 }
